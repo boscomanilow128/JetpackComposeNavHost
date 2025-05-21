246e44679f6e9061bd306fd20e42230b6d44c70f
Bosco Tsin <boscomanilow128@gmail.com>
2025-05-22 05:22:47 +0800

    First Commit

---
+++ app/src/main/java/com/boscotsin/jetpackcomposenavhost/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.boscotsin.jetpackcomposenavhost
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.boscotsin.jetpackcomposenavhost.ui.theme.JetpackComposeNavHostTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeNavHostTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Navigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeNavHostTheme {
+        Greeting("Android")
+    }
+}
