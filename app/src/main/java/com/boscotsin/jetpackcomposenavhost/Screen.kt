13caa99d835f34da8e869ea4ef40a46a357afe7a
Bosco Tsin <boscomanilow128@gmail.com>
2025-05-25 02:09:06 +0800

    Minimal Viable Codes

--- app/src/main/java/com/boscotsin/jetpackcomposenavhost/Screen.kt
+++ app/src/main/java/com/boscotsin/jetpackcomposenavhost/Screen.kt
@@ -4,12 +4,4 @@ sealed class Screen(val route: String) {
     object MainScreen: Screen("main_screen")
     object DetailScreen: Screen("detail_screen")
 
-    fun withArgs(vararg args: String): String {
-        return buildString {
-            append(route)
-            args.forEach { arg ->
-                append("/$arg")
-            }
-        }
-    }
 }
